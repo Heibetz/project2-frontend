dca89ff093e99d336be1a109f0504d1f27f11c70
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-10-01 17:00:49 -0500

    Add Correct Configs

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/courses-t5'
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      '/courses-t5': {
+        target: baseURL,
         changeOrigin: true,
         secure: false,
       },
