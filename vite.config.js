eada6e696e940fe672885a96b664f4b772a4acca
Hank Heiselbetz <hheiselbetz@gmail.com>
2025-10-01 17:23:32 -0500

    update vue config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/courses-t5'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/courses-t5/'
 export default defineConfig({
   plugins: [vue()],
   server: {
